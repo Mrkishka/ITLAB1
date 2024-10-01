4418dcd8a5ddb1ee7d679f7fca5d3709fcd8e9a8
Miltov Danil <Dmiltov@bk.ru>
2024-10-01 11:37:31 +0700

    Add js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Переменные
+let numberVariable = 42;        // number
+let stringVariable = "Привет!"; // string
+let booleanVariable = true;     // boolean
+let nullVariable = null;        // null
+let undefinedVariable;          // undefined
+let objectVariable = { name: "Alice", age: 30 }; // object
+
+// Константы
+const numberConstant = 100;     // number
+const stringConstant = "Константа"; // string
+const booleanConstant = false;  // boolean
+const nullConstant = null;      // null
+const undefinedConstant = undefined; // undefined
+const objectConstant = { name: "Pudge", KDA: 0 / 7 / 3 }; // object
